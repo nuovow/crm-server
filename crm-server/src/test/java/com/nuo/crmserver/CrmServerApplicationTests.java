4435d7d6d7fb6f79549c4cf7a50a38c31e7f6418
nuo <1690931096@qq.com>
2026-08-19 13:26:05 +0800

    后端项目初始化

---
+++ crm-server/src/test/java/com/nuo/crmserver/CrmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuo.crmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
